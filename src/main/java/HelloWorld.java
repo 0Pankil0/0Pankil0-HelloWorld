8603a9bb61f8a66946a755dd84c76211030a0500
0Pankil0 <02Pankil@gmail.com>
2023-02-22 16:55:15 +0000

    Coding Lab: Hello World

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] arg){
         System.out.println(" Namaste, How are you ? ");
+        System.out.println( " Hello world ! ");
     }
 
 }
